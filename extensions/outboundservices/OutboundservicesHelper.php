4f1091d4102690ac2846027bab6321d57ffd1cd9
Reik Mueller <reik.mueller@uni-leipzig.de>
2015-11-27 10:27:17 +0100

    module for exposing arbitrary data to the www

---
+++ extensions/outboundservices/OutboundservicesHelper.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2013, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+/**
+ * Helper class for the meta-data component.
+ *
+ * - register the tab for all navigations except the instances list
+ *   (this should be undone if the history can be created from a Query2 too)
+ *
+ * @category   OntoWiki
+ * @package    Extensions_Outboundservices
+ * @author     Reik Mueller
+ * @copyright  Copyright (c) 2015, {@link http://amsl.technology/}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+class OutboundservicesHelper extends OntoWiki_Component_Helper
+{
+    public function init()
+    {
+    }
+}
+
