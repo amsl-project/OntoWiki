57acd6e5dbe965daec7e34695526bcb41a2d7c93
Reik Mueller <reik.mueller@uni-leipzig.de>
2016-03-04 11:20:57 +0100

    listview: sort results

--- extensions/listview/ListviewController.php
+++ extensions/listview/ListviewController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Listexporter component controller. This controller is used for demonstration and export
  * purposes of the listexporter.
@@ -14,13 +15,9 @@ class ListviewController extends OntoWiki_Controller_Component
 
     public function listAction()
     {
-        $translate = $this->_owApp->translate;
-
         $this->view->placeholder('main.window.title')->set('Listview');
         $this->addModuleContext('main.window.listmodules.list');
         OntoWiki::getInstance()->getNavigation()->disableNavigation();
-
-        $query = $_GET['query'];
 
         $titleHelper = new OntoWiki_Model_TitleHelper($this->_owApp->selectedModel);
         $subjectUri = $_GET['subject'];
@@ -28,10 +25,12 @@ class ListviewController extends OntoWiki_Controller_Component
 
         $propertyUri = $_GET['property'];
         $propertyTitle = $titleHelper->getTitle($propertyUri);
-        //query selected model
+
+        $query = $_GET['query'];
         $result = $this->_owApp->selectedModel->sparqlQuery(
             $query
         );
+        usort($result, array('ListviewController', 'cmp'));
 
         $this->view->placeholder('main.window.title')->set($this->_owApp->translate->_('List-view for Resource') . ': ' . $subjectTitle);
         $this->view->subjectUri = $subjectUri;
@@ -40,5 +39,10 @@ class ListviewController extends OntoWiki_Controller_Component
         $this->view->propertyTitle = $propertyTitle;
         $this->view->results = $result;
         return;
+    }
+
+    private function cmp($a, $b)
+    {
+        return strcasecmp($a['value'], $b['value']);
     }
 }
